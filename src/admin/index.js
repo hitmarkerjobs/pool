90ecbc10536b2a83d2ec6af12f916991e6f92293
James Nuttall <james@ghostwhite.net>
2020-02-05 21:56:56 +0000

    New config

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,6 +1,6 @@
 import cms from "netlify-cms";
-import controlComponent from './idWidgetControl';
-import previewComponent from './idWidgetPreview';
+import controlComponent from './widgets/idWidgetControl';
+import previewComponent from './widgets/idWidgetPreview';
 
 // Register the widget. This lets NetlifyCMS know about our custom widget
 cms.registerWidget('id', controlComponent, previewComponent);
