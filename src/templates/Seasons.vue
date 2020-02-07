e2791c9013236fddc558766a90d63d1be3449710
James Nuttall <james@ghostwhite.net>
2020-02-07 10:22:31 +0000

    Season players

--- src/templates/Seasons.vue
+++ src/templates/Seasons.vue
@@ -12,8 +12,44 @@
       </g-link>
     </div>
 
-    {{ $page.season.title }}
-    {{ $page.season.fixtures }}
+    {{ seasonPlayers }}
+
+    <table class="table-auto w-full border border-gray-700 text-center">
+      <thead>
+        <tr class="bg-gray-800">
+          <th width="1"></th>
+          <th></th>
+          <th class="font-bold p-2">P</th>
+          <th class="font-bold p-2">W</th>
+          <th class="font-bold p-2">L</th>      
+          <th class="font-bold p-2">Pts</th>
+        </tr>
+      </thead>
+      <tbody>
+<!--        <tr 
+          v-for="(edge, index) in players"
+          :key="edge.node.id"
+          class="border-t border-gray-700"
+        >
+          <td class="px-4">{{ index+1 }}</td>
+
+          <td class="text-left pr-2">
+            <div class="flex items-center">
+              <div class="flex-none w-16 h-16">
+                <g-image :src="edge.node.photo" />
+              </div>
+
+              <div class="font-bold ml-4">{{ edge.node.name }}</div>
+            </div>
+          </td>
+          
+          <td class="px-2">{{ edge.node.wins + edge.node.losses }}</td>
+          <td class="px-2">{{ edge.node.wins }}</td>
+          <td class="px-2">{{ edge.node.losses }}</td>
+          <td class="px-2">{{ edge.node.wins * 3 }}</td>
+        </tr> -->
+      </tbody>
+    </table>
   </Layout>
 </template>
 
@@ -21,6 +57,24 @@
 export default {
   metaInfo: {
     title: 'Hitmarker Pool'
+  },
+  computed: {
+    seasonPlayers () {
+      let allPlayers = this.$page.players.edges
+        .map(p => p.node)
+        .reduce((ps, p) => {
+          ps[p.id] = p;
+          return ps;
+        }, {});
+
+      let playerIds = this.$page.season.fixtures.map(f => {
+        f = f.fixture;
+        return [f.player1, f.player2];
+      }).flat().filter((v, i, a) => a.indexOf(v) === i); 
+
+
+      return playerIds.map(p => allPlayers[p]);
+    }
   }
 }
 </script>
@@ -33,6 +87,15 @@ export default {
           id
           title
           path
+        }
+      }
+    }
+    players:allPlayer {
+      edges {
+        node {
+          id
+          name
+          photo
         }
       }
     }
