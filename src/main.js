df658d53f8496298b0b1a02bfdba48979fed2645
James Nuttall <jamesnuttall98@gmail.com>
2020-02-09 19:42:23 +0000

    No index

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import DefaultLayout from '~/layouts/Default.vue'
 import "./assets/css/app.css";
 
 export default function (Vue, { router, head, isClient }) {
-  Vue.component('Layout', DefaultLayout)
+  Vue.component('Layout', DefaultLayout);
 
-  head.bodyAttrs = { class: 'bg-gray-900 text-gray-100 antialiased' }
+  head.bodyAttrs = { class: 'bg-gray-900 text-gray-100 antialiased' };
+  head.meta.push({
+    name: "robots",
+    content: "no-index"
+  });
 }
